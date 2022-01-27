7818c400bc2c1e755143d30a8eba3bf661bedd21
Sebestyen Marton <sdgmarton@gmail.com>
2022-01-27 20:28:52 +0200

    finalized navbar

--- src/components/navbar-section/red-navbar-section.js
+++ src/components/navbar-section/red-navbar-section.js
@@ -7,17 +7,29 @@ const RedNavbarSection = () => {
     <div className="navbar-section">
       <div className="navbar">
         <div className="navbar-logo" />
-        <div className="navbar-line" />
+        <div className="navbar-line first" />
         <ul className="navbar-menu">
-          <li>Főoldal</li>
-          <li>Információk</li>
-          <li>Gyik</li>
-          <li>Média</li>
+          <li>
+            Főoldal
+            <div className="navbar-background elso active" />
+          </li>
+          <li>
+            Információk
+            <div className="navbar-background masodik" />
+          </li>
+          <li>
+            Gyik
+            <div className="navbar-background harmadik" />
+          </li>
+          <li>
+            Média
+            <div className="navbar-background negyedik" />
+          </li>
         </ul>
         <div className="navbar-line" />
         <div className="navbar-top-right">S.Márton</div>
       </div>
-      <div className="navbar-line"></div>
+      <div className="navbar-center"></div>
       <div className="navbar-bottom"></div>
     </div>
   );
